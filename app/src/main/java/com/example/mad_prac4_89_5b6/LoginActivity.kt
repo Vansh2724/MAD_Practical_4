79f8f296465ebd227b9d7a01fdf651baad60c942
Vansh Patel <vanshpatel2724@gmail.com>
2023-08-24 06:57:58 +0530

    24/8/23

--- app/src/main/java/com/example/mad_prac4_89_5b6/LoginActivity.kt
+++ app/src/main/java/com/example/mad_prac4_89_5b6/LoginActivity.kt
@@ -1,11 +1,18 @@
 package com.example.mad_prac4_89_5b6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        val btnsignup: Button = findViewById(R.id.btn_signup)
+        val i = Intent(this,RegistrationActivity::class.java)
+        btnsignup.setOnClickListener {
+            startActivity(i)
+        }
     }
 }
--- app/src/main/java/com/example/mad_prac4_89_5b6/RegistrationActivity.kt
+++ app/src/main/java/com/example/mad_prac4_89_5b6/RegistrationActivity.kt
@@ -1,11 +1,18 @@
 package com.example.mad_prac4_89_5b6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        val btnlogin: Button = findViewById(R.id.btn_login)
+        val i = Intent(this,LoginActivity::class.java)
+        btnlogin.setOnClickListener {
+            startActivity(i)
+        }
     }
 }
